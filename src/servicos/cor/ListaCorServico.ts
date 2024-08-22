7d457d4164b7bd32de621f19170ebeac2b9ba685
Wilson Ramos <hupcontato@gmail.com>
2024-08-22 07:44:06 -0300

    mudanças 22/08

--- src/servicos/cor/ListaCorServico.ts
+++ src/servicos/cor/ListaCorServico.ts
@@ -2,7 +2,14 @@ import prismaClient from '../../prisma';
 
 class ListaCorServico {
   async execute() {
-    return await prismaClient.cor.findMany();
+    return await prismaClient.cor.findMany({
+      select: {
+        id: true,
+        codigo: true,
+        nome: true,
+        produto: { select: { estoque: true, saida: true } }
+      }
+    });
   }
 }
 
