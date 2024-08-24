071a3122141d26ef16f6f7437d526ac04f1696be
Wilson Ramos <hupcontato@gmail.com>
2024-08-24 07:43:24 -0300

    mudanças 24/08

--- src/servicos/cliente/AtualizaClienteServico.ts
+++ src/servicos/cliente/AtualizaClienteServico.ts
@@ -1,19 +1,19 @@
 import prismaclient from "../../prisma";
 
 interface DadosDoCliente {
-    nome: string, endereco: string, bairro: string, cidade: string, estado: string, whatsapp: string, dataNascimento: string, inscricaoEstadualRg: string, CEP: string, clienteID: string
+    cpf_cnpj: string, nome: string, endereco: string, bairro: string, cidade: string, estado: string, whatsapp: string, dataNascimento: string, inscricaoEstadualRg: string, CEP: string, clienteID: string
 }
 
 // Movimentação para retirada definitiva do estoque
 class AtualizaClienteServico {
-    async execute({ nome, endereco, bairro, cidade, estado, whatsapp, dataNascimento, inscricaoEstadualRg, CEP, clienteID }: DadosDoCliente) {
+    async execute({ cpf_cnpj,nome, endereco, bairro, cidade, estado, whatsapp, dataNascimento, inscricaoEstadualRg, CEP, clienteID }: DadosDoCliente) {
 
         await prismaclient.cliente.updateMany({
             where: {
                 id: clienteID,
             },
             data: {
-                nome, endereco, bairro, cidade, estado, whatsapp, dataNascimento, inscricaoEstadualRg, CEP
+                cpf_cnpj,nome, endereco, bairro, cidade, estado, whatsapp, dataNascimento, inscricaoEstadualRg, CEP
             }
         })
 
